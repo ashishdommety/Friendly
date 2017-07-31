f14c87e3de7e7db8328f3cedd6ba626bbc908b20
Ashish Dommety <ashishuiux@gmail.com>
2017-07-31 14:52:26 -0700

    Added the survey form dynamically and made the post request

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = 3000;
 // const htmlRoutes = require("./app/routing/htmlRoutes.js");
 // const apiRoutes = require("./app/routing/apiRoutes.js");
 
+var people = [];
+
+// NOTE: html routes
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
@@ -16,12 +19,18 @@ app.get("/", function(req,res){
   res.sendFile(path.join(__dirname, "./app/public/home.html"));
 })
 
+// NOTE: api routes
 app.get("/api/friends", function(req,res){
   res.json();
 })
 
 app.post("/api/friends", function(req,res){
-  res.json();
+  var newFriend = req.body;
+  console.log(newFriend);
+  // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  // console.log(newFriend);
+  // people.push(newFriend);
+  // res.json(newFriend);
 })
 
 app.listen(PORT, function(){
